107b0baedd6e3def025296231fa08c65ecd8b889
Pedroni Gilbran <pedronibt45@gmail.com>
2025-12-29 22:48:53 +0700

    perbaikan toast

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -25,7 +25,10 @@ export default function LoginPage() {
     if (data.token) {
       localStorage.setItem("token", data.token);
       toast.success("Login berhasil");
-      router.push("/users");
+
+      setTimeout(() => {
+        router.push("/users");
+      }, 1500);
     } else {
       toast.error(data.error || "Login gagal");
     }
@@ -35,53 +38,57 @@ export default function LoginPage() {
 
   return (
     <>
-    <div className="min-h-screen flex items-center justify-center">
-      <ToastContainer theme="dark" />
+      <div className="min-h-screen flex items-center justify-center">
+        <ToastContainer theme="dark" />
 
-      <div className="border p-6 rounded-2xl w-90">
-        <h1 className="text-center text-black text-2xl py-4 font-extrabold">Welcome Back</h1>
-        <p className="text-center text-black-50 p-4">please enter your details to sign in</p>
+        <div className="border p-6 rounded-2xl w-90">
+          <h1 className="text-center text-black text-2xl py-4 font-extrabold">
+            Welcome Back
+          </h1>
+          <p className="text-center text-black-50 p-4">
+            please enter your details to sign in
+          </p>
 
-        <div className="my-4">
-        <p className="font-bold py-2">Email Address</p>
-        <input
-          placeholder="Enter Your Email"
-          className="border w-full mb-3 px-3 py-2 rounded-sm"
-          onChange={(e) => setEmail(e.target.value)} 
-        />
-        </div>
+          <div className="my-4">
+            <p className="font-bold py-2">Email Address</p>
+            <input
+              placeholder="Enter Your Email"
+              className="border w-full mb-3 px-3 py-2 rounded-sm"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
 
-        <div className="my-4">
-        <p className="font-bold py-2">Password</p>
-        <input
-          type="password"
-          placeholder="Password"
-          className="border w-full mb-4 px-3 py-2 rounded-sm"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        </div>
+          <div className="my-4">
+            <p className="font-bold py-2">Password</p>
+            <input
+              type="password"
+              placeholder="Password"
+              className="border w-full mb-4 px-3 py-2 rounded-sm"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
 
-        <div className="my-4">
-        <button
-          onClick={submitLogin}
-          disabled={loading}
-          className="cursor-pointer bg-black text-white w-full py-2"
-        >
-          {loading ? "Loading..." : "Sign In"}
-        </button>
-        </div>
+          <div className="my-4">
+            <button
+              onClick={submitLogin}
+              disabled={loading}
+              className="cursor-pointer bg-black text-white w-full py-2"
+            >
+              {loading ? "Loading..." : "Sign In"}
+            </button>
+          </div>
 
-                <div className="text-center text-sm mt-4">
-          Don't have an account?
-          <Link
-            href="/register"
-            className="ml-1 font-semibold text-black hover:underline"
-          >
-            Sign up
-          </Link>
+          <div className="text-center text-sm mt-4">
+            Don't have an account?
+            <Link
+              href="/register"
+              className="ml-1 font-semibold text-black hover:underline"
+            >
+              Sign up
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
